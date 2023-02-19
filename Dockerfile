9f9e5c040819711a44d5a29c7f506ed22ff13004
EllaMozes <ellamozes@gmail.com>
2023-02-19 14:44:01 +0200

    #1

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM robd003/python3.9
+RUN rm /bin/sh && ln -s /bin/bash /bin/sh
+
+# RUN apt-get update && apt-get upgrade -y && apt-get install python3.9 python3.9-venv -y 
+EXPOSE 8080
+WORKDIR /usr/app
+COPY requirments.txt requirments.txt  
+RUN python3.9 -m venv venv
+RUN source venv/bin/activate
+RUN pip install -r requirments.txt
+COPY app.py ./
+
+CMD ["python3.9", "app.py"]
---
+++ app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import time
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+START = time.time()
+
+def elapsed():
+    running = time.time() - START
+    minutes, seconds = divmod(running, 60)
+    hours, minutes = divmod(minutes, 60)
+    return "%d:%02d:%02d" % (hours, minutes, seconds)
+
+@app.route('/')
+def root():
+    return "Hello World (Python)! (up %s)\n" % elapsed()
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'Hello %s' % escape(username)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
