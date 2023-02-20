3bd2b7e650cf3746af8cf41fc144c974a14e95ef
EllaMozes <ellamozes@gmail.com>
2023-02-20 11:11:28 +0200

    #1

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def root():
 def show_user_profile(username):
     # show the user profile for that user
     return 'Hello %s' % escape(username)
-    
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
